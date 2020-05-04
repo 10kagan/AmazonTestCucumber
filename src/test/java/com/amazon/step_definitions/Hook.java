eca15adf2351b0fb11bf1047b6d032767bcdab44
10kagan <kaganalperen@gmail.com>
2020-05-04 00:38:49 -0500

    added test cases

--- src/test/java/com/amazon/step_definitions/Hook.java
+++ src/test/java/com/amazon/step_definitions/Hook.java
@@ -1,4 +1,17 @@
 package com.amazon.step_definitions;
 
+import cucumber.api.Scenario;
+import org.junit.After;
+import org.junit.Before;
+
 public class Hook {
+    @Before
+    public void setup(Scenario scenario){
+
+    }
+
+    @After
+    public void teardown(Scenario scenario){
+
+    }
 }
--- src/test/java/com/amazon/step_definitions/book.java
+++ src/test/java/com/amazon/step_definitions/book.java
@@ -1,9 +1,36 @@
 package com.amazon.step_definitions;
 
 import com.amazon.utilities.Pages;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class book {
+import java.util.concurrent.TimeUnit;
+
+ class Book {
 
     Pages pages = new Pages();
+
+    @Given("Search for the book Ender's Game by Orson Scott Card")
+    public void search_for_the_book_Enders_Game_by_Orson_Scott_Card(){
+
+    }
+    @And("Click on the book from the search")
+    public void click_on_the_book_from_the_search(){
+
+    }
+    @Then("Add the book to your cart")
+    public void add_the_book_to_your_cart(){
+
+    }
+    @Then("Navigate to your cart and verify the book is there")
+    public void navigate_to_your_cart_and_verify_the_book_is_there(){
+
+    }
+    @Then("Remove the book from the cart and verify that it is no longer present")
+    public void remove_the_book_from_the_cart_and_verify_that_it_is_no_longer_present(){
+
+    }
 
 }
